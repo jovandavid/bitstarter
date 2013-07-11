1adddc12d0bc318f58c1af2e79b675a519c64cce
Jovan Rebolledo <contact@jovandavid.net>
2013-07-11 03:47:06 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(26);
+buffer.write(fs.readFileSync('index.html'), "utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ //fs.readFileSync('index.html');
+ response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
